4253c49b209141cd5c5c2899297a88bdcac8a678
eseom <me@eseom.org>
2017-04-28 01:12:05 +0900

    recover plugin options

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,13 @@ export interface Options {
 declare module 'hapi' {
   export interface Server {
     origRoute: any
+    __proto__: any
   }
 }
 
-const register: IRegister = (server, pOptions, next: () => {}) => {
-  const origRoute = server.root.route
+const register: IRegister = (plugin, pOptions, next: () => {}) => {
+  const server = pOptions.server
+  const origRoute = server.route
   const innerRoute = (options: Options) => {
     if (options.handler) {
       if (options.handler instanceof Function) {
@@ -37,7 +39,7 @@ const register: IRegister = (server, pOptions, next: () => {}) => {
     return origRoute.apply(server, [options])
   }
 
-  server.root.route = (options: Object) => {
+  server.route = (options: Object) => {
     if (Array.isArray(options)) {
       return options.map(option => innerRoute(option))
     }
